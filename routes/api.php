a97d9be3df8d5cdc2c44fcbe5e402ef9cfdf7308
davidnineroc <1033404553@qq.com>
2019-11-22 18:44:26 +0800

    移除dd打印

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,6 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 
 $app->get('/', function (Request $request, Response $response) {
    
-    dd($request->getQueryParams());
    $response->getBody()->write('hello, world');
    return $response;
 });
